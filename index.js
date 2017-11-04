0713ce95dbc0c94a860ae7de143f581556754888
Jennifer Haddock <jentx123@hotmail.com>
2017-11-04 02:28:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,3 +10,17 @@ function distanceFromHqInFeet (pickup_block){
 function distanceTravelledInFeet (pickup_block, destination_block){
   return Math.abs((pickup_block - destination_block) * 264);
 }
+
+function calculatesFarePrice (pickup_block, destination_block){
+  const distance = distanceTravelledInFeet(pickup_block, destination_block);
+  switch(distance){
+    case (distance > 2500):
+      return 'cannot travel that far';
+    case (distance > 2000):
+      return 25;
+    case (distance > 400);
+      return distance * 0.02;
+    default:
+      return 0;
+  }
+}
