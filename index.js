d53b46c0c35d740cc209e85cd866b538ecea55a8
Jennifer Haddock <jentx123@hotmail.com>
2017-11-04 02:19:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,3 +2,7 @@
 function distanceFromHqInBlocks (pickup_block){
   return Math.abs(pickup_block - 42);
 }
+
+function distanceFromHqInFeet (pickup_block){
+  return distanceFromHqInBlocks(pickup_block) * 264;
+}
